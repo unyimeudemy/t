3ea64eac9567653ee8843dc35565f579d9fe65f3
unyimeudoh <unyimeudemy20@gmail.com>
2024-08-23 10:58:12 +0100

    chore: test deployment

--- src/util/axios.js
+++ src/util/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-    baseURL: "http://localhost:8080/api/v1/url",
+    // baseURL: "http://localhost:8080/api/v1/url",
+    baseURL: "https://tinyurl-zgzb.onrender.com",
 })
 export default Axios;
